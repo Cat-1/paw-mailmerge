d4deab012bce78d18f956c2e8e3231168104ce1a
Wenhao Chen <97068431+regular-polygon@users.noreply.github.com>
2024-01-24 16:15:35 -0800

    Write basic text input form for template composition

--- react-paw-mailmerge/src/routes/WriteTemplate.tsx
+++ react-paw-mailmerge/src/routes/WriteTemplate.tsx
@@ -1,8 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
+import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button";
+
+
 const WriteTemplate: React.FC = () => {
+    const [templateValue, setTemplateValue] = useState<string>('');
+
+    const handleTemplateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTemplateValue(event.target.value);
+    }
+
+    const handleTemplateSubmit = () => {
+        console.log("Submitting template:", templateValue)
+    }
+
     return (
         <div>
-            <h1>Compose Template</h1>
+            <Form onSubmit={handleTemplateSubmit}>
+                <Form.Group className="mb-3">
+                    <Form.Label>Compose Template</Form.Label>
+                    <Form.Control 
+                        value={templateValue}
+                        onChange={handleTemplateChange}
+                        placeholder="Hello {{name}}, ..."
+                        as="textarea" 
+                        rows={6}
+                    />
+                </Form.Group>
+                <Button type="button" onClick={handleTemplateSubmit}>Mail Merge</Button>
+            </Form>
         </div>
     )
 }
