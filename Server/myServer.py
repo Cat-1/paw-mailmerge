dafa6c406d29ac77b62d1ed5a56933431a490fb0
Cat-1 <joosc@oregonstate.edu>
2024-01-27 16:07:15 -0500

    Serves react app

--- Server/myServer.py
+++ Server/myServer.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/', static_folder='.\\..\\react-paw-mailmerge\\build')
 
 
-@app.route("/")
-def hello_world():
+# Referenced this blog
+# https://blog.miguelgrinberg.com/post/how-to-deploy-a-react--flask-project
+@app.route('/')
+def serve_page():
+    return send_from_directory(app.static_folder, 'index.html')
 
-    return "<p>Hello, World!</p>"
+
+
