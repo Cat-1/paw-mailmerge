a7e6ccee518d5405614c0edf3d75b80746d5eb56
Cat-1 <joosc@oregonstate.edu>
2024-01-29 19:14:58 -0500

    CR fix

--- Server/myServer.py
+++ Server/myServer.py
@@ -1,10 +1,13 @@
 from flask import Flask, send_from_directory
-
-app = Flask(__name__, static_url_path='/', static_folder='.\\..\\react-paw-mailmerge\\build')
-
+from os import path
 
 # Referenced this blog
 # https://blog.miguelgrinberg.com/post/how-to-deploy-a-react--flask-project
+
+react_build_folder = path.join('.', '..', 'react-paw-mailmerge', 'build')
+app = Flask(__name__, static_url_path='/', static_folder=react_build_folder)
+
+
 @app.route('/')
 def serve_page():
     return send_from_directory(app.static_folder, 'index.html')
