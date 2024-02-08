473b07fc88464e74c5728fc6091aad83da25e989
Wenhao Chen <97068431+regular-polygon@users.noreply.github.com>
2024-02-07 18:19:12 -0800

    Create a form to let users compose a template. Disable text input after the template is submitted. Add edit and discard changes buttons.

--- react-paw-mailmerge/src/components/WriteTemplate.tsx
+++ react-paw-mailmerge/src/components/WriteTemplate.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Form from 'react-bootstrap/Form';
 import Button from "react-bootstrap/Button";
 
@@ -9,13 +9,23 @@ interface WriteTemplateProps {
 }
 
 const WriteTemplate: React.FC<WriteTemplateProps> = ({parsedData, template, setTemplate}) => {
+    const [editingEnabled, setEditingEnabled] = useState<boolean>(true); // enable / disable form editing
+    const [currentInput, setCurrentInput] = useState<string>('');
+    const [previousInput, setPreviousInput] = useState<string>('');  // when form changes are discarded, restore to previousInput
 
     const handleTemplateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTemplate(event.target.value);
+        setCurrentInput(event.target.value);
     }
 
     const handleTemplateSubmit = () => {
-        console.log("Submitting template:", template)
+        console.log("Submitting template:", currentInput);
+        setPreviousInput(currentInput);
+        setTemplate(currentInput);  // state lifting
+        setEditingEnabled(false);
+    }
+
+    const handleTemplateDiscardChanges = () => {
+        setCurrentInput(previousInput);
     }
 
     return (
@@ -24,14 +34,19 @@ const WriteTemplate: React.FC<WriteTemplateProps> = ({parsedData, template, setT
                 <Form.Group className="mb-3">
                     <Form.Label>Compose Template</Form.Label>
                     <Form.Control 
-                        value={template}
+                        value={currentInput}
                         onChange={handleTemplateChange}
                         placeholder="Hello {{name}}, ..."
                         as="textarea" 
                         rows={6}
+                        disabled={!editingEnabled}
                     />
                 </Form.Group>
-                <Button type="button" onClick={handleTemplateSubmit}>Mail Merge</Button>
+                <div>
+                    <Button type="button" className="me-2" onClick={handleTemplateSubmit} hidden={!editingEnabled}>Save Template</Button>
+                    <Button type="button" variant="danger" onClick={handleTemplateDiscardChanges} hidden={!editingEnabled || previousInput === currentInput}>Discard Changes</Button>
+                    <Button type="button" variant="warning" onClick={() => {setEditingEnabled(true)}} hidden={editingEnabled}>Edit Template</Button>
+                </div>
             </Form>
         </div>
     )
