05fff820c78bd896fdfe33bed1098fb35018e664
Cat-1 <joosc@oregonstate.edu>
2024-02-25 21:35:15 -0500

    Backend Change

--- Test_Backend.py
+++
@@ -1,6 +0,0 @@
-import Server.myServer
-
-
-def test_hello_world():
-    assert Server.myServer.hello_world() == "<p>Hello, World!</p>"
-
---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+app.set('port', 51231);
+
+var reactBuildFolder = path.join(__dirname, 'react-paw-mailmerge', 'build', 'index.html')
+app.use(express.static(path.join(__dirname, 'react-paw-mailmerge','build')));
+
+
+// standard listening
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
